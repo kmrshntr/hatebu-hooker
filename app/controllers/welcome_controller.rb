e718d7741686c64d904a379df87b1037d46ca635
kimura <kimura@enigmo.co.jp>
2014-02-17 20:47:56 +0900

    Post to Slack

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,9 +8,23 @@ class WelcomeController < ApplicationController
   end
 
   def hook
-    p params
-	respond_to do |format|
-        format.json { head :no_content }
+    if user = User.where(hatena_id: params[:username]).first && user.hatena_bookmark_web_hook_key == params[:key] && params[:status] == "add"
+      conn = Faraday.new(:url => 'https://slack.com')
+      res = conn.get("/api/users.list?token=#{user.slack_token}")
+      members = JSON.parse(res.body)['members']
+      username = members.detect{|member| member['profile']['email'] == user.email}['name']
+      text = ""
+      if params[:comment].present?
+        text += params[:comment].to_s + " / "
+      end
+      if params[:title].present?
+        text += '"' + params[:title]+'" '
+      end
+      if params[:url].present?
+        text += params[:url]
+      end
+      conn.post '/api/chat.postMessage', { token: user.slack_token, channel: 'G024ZC07C', text: 'kimura api text', username: username}
     end
+    render text: 'OK'
   end
 end
