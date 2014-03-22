b0e916784ffc05924d0456e6ea5e626aa0199ca7
Shintaro Kimura <kmrshntr@gmail.com>
2014-03-23 02:17:46 +0900

    アイコンを表示

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,9 +14,17 @@ class WelcomeController < ApplicationController
         text += "#{params[:comment]} / " if params[:comment].present?
         text += "\"#{params[:title]}\"" if params[:title].present?
         text += params[:url] if params[:url].present?
-
         conn = Faraday.new(:url => 'https://slack.com')
-        conn.post '/api/chat.postMessage', { token: user.slack_token, channel: 'C024YQKLA', text: text, username: user.name}
+        user_id = JSON.parse(conn.get('/api/auth.test', token: user.slack_token).body)['user_id']
+        members = JSON.parse(conn.get('/api/users.list', token: user.slack_token).body)['members']
+        slack_user = members.detect{|m|m["id"] == user_id}
+        result = conn.post '/api/chat.postMessage', { token: user.slack_token,
+          channel: 'C024YQKLA', text: text,
+          username: slack_user['name'],
+          icon_url: slack_user['profile']['image_48'],
+          unfurl_links: 1
+        }
+        logger.info(result.inspect);
       end
     end
     render text: 'OK'
